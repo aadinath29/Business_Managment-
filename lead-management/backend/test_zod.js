5e729664177894fa6c32b5bfe87cdd6c1d292ca4
aadinath29 <aadinathbmagar75@gmail.com>
2026-07-13 14:01:28 +0530

    feat - removing the test files

--- lead-management/backend/test_zod.js
+++
@@ -1,8 +0,0 @@
-const { z } = require('zod');
-
-const schema = z.object({
-  sortBy: z.enum(['created_at', 'branch_name']).optional().default('created_at'),
-  sortOrder: z.enum(['asc', 'desc', 'ASC', 'DESC']).optional().transform(val => val ? val.toUpperCase() : 'DESC')
-});
-
-console.log(schema.safeParse({}));
